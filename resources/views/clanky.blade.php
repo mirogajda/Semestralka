35b9c51c448166545eed530e85d8be10a035007f
miros <miro@miro.miro>
2022-01-17 13:33:29 +0100

    pridavanie clankov a dalsie opravy2

--- resources/views/clanky.blade.php
+++ resources/views/clanky.blade.php
@@ -3,8 +3,8 @@
     <form class="logContainer">
         @foreach($articles as $article)
                 <div class="article-title">{{$article->title}}</div>
-                <div class="popis2">{{$article->content}}</div>
-                <div>Článok bol naposledy upravený: {{$article->updated_at}}</div>
+                <div class="popis4">{{$article->content}}</div>
+                <div class="clanok-info">Článok bol naposledy upravený: {{$article->updated_at}}</div>
         @endforeach
     </form>
 @endsection
